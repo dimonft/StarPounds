25c208a612e12b8f0a098b280ee2fce4b1249de2
LittleVulpine <141291111+LittleVulpine@users.noreply.github.com>
2025-02-27 14:04:56 +1000

    Monster script cleanup

--- StarPounds/scripts/starpounds/starpounds_monster.lua
+++ StarPounds/scripts/starpounds/starpounds_monster.lua
@@ -2,46 +2,33 @@
 local function nullFunction()
 end
 -- Old functions. (we call these in functons we replace)
-local init_old = init or nullFunction
 local update_old = update or nullFunction
 local uninit_old = uninit or nullFunction
--- Run on load.
-function starPoundsInit()
-  require "/scripts/starpounds/starpounds.lua"
-  storage.starPounds = sb.jsonMerge(starPounds.baseData, storage.starPounds)
-  -- This is stupid, but prevents 'null' data being saved.
-  getmetatable(storage.starPounds).__nils = {}
-  -- Used in functions for detection.
-  starPounds.type = "monster"
-  starPounds.moduleInit("base")
-  -- Setup message handlers
-  starPounds.messageHandlers()
-  -- Reload whenever the entity loads in/beams/etc.
-  starPounds.statCache = {}
-  starPounds.statCacheTimer = starPounds.settings.statCacheTimer
-  starPounds.parseSkills()
-  starPounds.parseStats()
-  starPounds.accessoryModifiers = starPounds.getAccessoryModifiers()
-  starPounds.moduleInit({"entity", "monster", "vore"})
-  starPounds.effectInit()
-  starPounds.setWeight(storage.starPounds.weight)
+-- Monsters load this from a behaviour script, so we can't just hook into init since it's already run.
+require "/scripts/starpounds/starpounds.lua"
+storage.starPounds = sb.jsonMerge(starPounds.baseData, storage.starPounds)
+-- This is stupid, but prevents 'null' data being saved.
+getmetatable(storage.starPounds).__nils = {}
+-- Used in functions for detection.
+starPounds.type = "monster"
+starPounds.moduleInit("base")
+-- Setup message handlers
+starPounds.messageHandlers()
+-- Reload whenever the entity loads in/beams/etc.
+starPounds.statCache = {}
+starPounds.statCacheTimer = starPounds.settings.statCacheTimer
+starPounds.parseSkills()
+starPounds.parseStats()
+starPounds.accessoryModifiers = starPounds.getAccessoryModifiers()
+starPounds.moduleInit({"entity", "monster", "vore"})
+starPounds.effectInit()
+starPounds.setWeight(storage.starPounds.weight)
 
-  starPounds.events:on("main:statChange", function()
-    starPounds.updateStats(true)
-  end)
-end
-
-function init()
-  -- Run old NPC/Monster stuff.
-  init_old()
-  starPoundsInit()
-end
+starPounds.events:on("main:statChange", function()
+  starPounds.updateStats(true)
+end)
 
 function update(dt)
-  if not starPounds then
-    require "/scripts/starpounds/starpounds.lua"
-    starPoundsInit()
-  end
   -- Run old NPC/Monster stuff.
   update_old(dt)
   -- Check promises.
