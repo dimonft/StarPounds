9052e3481962896cb8316e0aa2b109f52e844dc2
LittleVulpine <141291111+LittleVulpine@users.noreply.github.com>
2025-03-02 09:32:18 +1000

    Fix for monsters that directly load the script.

--- StarPounds/scripts/starpounds/starpounds_monster.lua
+++ StarPounds/scripts/starpounds/starpounds_monster.lua
@@ -1,30 +1,44 @@
--- Old functions. (we call these in functons we replace)
+-- Old functions. (we call these in functons we replace)-- Old functions. (we call these in functons we replace)
+local init_old = init or function() end
 local update_old = update or function(dt) end
 local uninit_old = uninit or function() end
--- Monsters load this from a behaviour script, so we can't just hook into init since it's already run.
-require "/scripts/starpounds/starpounds.lua"
-storage.starPounds = sb.jsonMerge(starPounds.baseData, storage.starPounds)
--- This is stupid, but prevents 'null' data being saved.
-getmetatable(storage.starPounds).__nils = {}
--- Used in functions for detection.
-starPounds.type = "monster"
-starPounds.moduleInit("base")
--- Setup message handlers
-starPounds.messageHandlers()
--- Reload whenever the entity loads in/beams/etc.
-starPounds.statCache = {}
-starPounds.statCacheTimer = starPounds.settings.statCacheTimer
-starPounds.parseSkills()
-starPounds.parseStats()
-starPounds.accessoryModifiers = starPounds.getAccessoryModifiers()
-starPounds.moduleInit({"entity", "monster", "vore"})
-starPounds.effectInit()
-starPounds.setWeight(storage.starPounds.weight)
-starPounds.weightMultiplier = 1
 
-starPounds.events:on("main:statChange", function()
-  starPounds.updateStats(true)
-end)
+function starPoundsInit()
+  -- Monsters usually load this from a behaviour script, so we can't just hook into init since it's already run.
+  require "/scripts/starpounds/starpounds.lua"
+  storage.starPounds = sb.jsonMerge(starPounds.baseData, storage.starPounds)
+  -- This is stupid, but prevents 'null' data being saved.
+  getmetatable(storage.starPounds).__nils = {}
+  -- Used in functions for detection.
+  starPounds.type = "monster"
+  starPounds.moduleInit("base")
+  -- Setup message handlers
+  starPounds.messageHandlers()
+  -- Reload whenever the entity loads in/beams/etc.
+  starPounds.statCache = {}
+  starPounds.statCacheTimer = starPounds.settings.statCacheTimer
+  starPounds.parseSkills()
+  starPounds.parseStats()
+  starPounds.accessoryModifiers = starPounds.getAccessoryModifiers()
+  starPounds.moduleInit({"entity", "monster", "vore"})
+  starPounds.effectInit()
+  starPounds.setWeight(storage.starPounds.weight)
+  starPounds.weightMultiplier = 1
+
+  starPounds.events:on("main:statChange", function()
+    starPounds.updateStats(true)
+  end)
+end
+
+-- Dirty.
+if root then
+  starPoundsInit()
+else
+  function init()
+    init_old()
+    starPoundsInit()
+  end
+end
 
 function update(dt)
   -- Run old NPC/Monster stuff.
