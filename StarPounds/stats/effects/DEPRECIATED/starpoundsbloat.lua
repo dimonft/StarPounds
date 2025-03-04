8c3ad9c40d0505e81ff5272a6e09c6293fe861ac
LittleVulpine <141291111+LittleVulpine@users.noreply.github.com>
2025-03-04 22:36:58 +1000

    Sail screaming

--- StarPounds/stats/effects/DEPRECIATED/starpoundsbloat.lua
+++ StarPounds/stats/effects/DEPRECIATED/starpoundsbloat.lua
@@ -2,6 +2,12 @@ function init()
   if effect.duration() > 0 then
     world.sendEntityMessage(entity.id(), "starPounds.feed", effect.duration(), "bloat")
   end
+  world.sendEntityMessage(entity.id(), "queueRadioMessage", {
+    messageId = "bloatWarning",
+    important = true,
+    unique = false,
+    text = "^red;------------WARNING------------\n^reset;^#ccbbff;StarPounds^reset; or an addon has applied the depreciated ^red;starpoundsbloat^reset; status effect.\nPlease inform the mod author(s).",
+  })
 end
 
 function update()
