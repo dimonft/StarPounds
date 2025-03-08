45a8cca4ba4929f306d75f114430c0398b1201ab
LittleVulpine <141291111+LittleVulpine@users.noreply.github.com>
2025-03-09 05:48:09 +1000

    Secret fix

--- StarPounds/stats/effects/starpoundssoundhandler/starpoundssoundhandler.lua
+++ StarPounds/stats/effects/starpoundssoundhandler/starpoundssoundhandler.lua
@@ -10,21 +10,30 @@ end
 
 function update(dt)
   effect.modifyDuration(dt)
+  -- Secret c:
+  local starPounds = getmetatable ''.starPounds
+  if secret and not starPounds.hasSkill("secret") then
+    effect.expire()
+  end
+  if not secret then
+    secret = starPounds.hasSkill("secret")
+  end
 end
 
 function playSound(soundPool, loops)
   local starPounds = getmetatable ''.starPounds
   if starPounds and starPounds.hasOption("disableSound") then return end
-
+  if secret then
+    animator.setSoundPool(soundPool, {"/sfx/starpounds/other/secret.ogg"})
+  end
   animator.playSound(soundPool, loops)
 end
 
 function setSoundVolume(soundPool, volume, rampTime)
   local starPounds = getmetatable ''.starPounds
   if starPounds then
-    if starPounds.hasSkill("secret") then
-      soundPool = "secret"
-      volume = (volume + 0.5) * 0.5
+    if secret then
+      volume = (volume + 0.35) * 0.5
     end
     if starPounds.hasOption("quietSounds") then
       volume = volume * 0.5
@@ -42,11 +51,6 @@ function setSoundPitch(soundPool, pitch, rampTime)
       if starPounds.hasOption("disableBelches") then return end
       if starPounds.hasOption("higherBelches") then pitch = pitch * 1.25 end
       if starPounds.hasOption("deeperBelches") then pitch = pitch * 0.75 end
-    end
-    -- Secret sound
-    if starPounds.hasSkill("secret") then
-      soundPool = "secret"
-      pitch = (pitch + 1) * 0.5
     end
   end
 
