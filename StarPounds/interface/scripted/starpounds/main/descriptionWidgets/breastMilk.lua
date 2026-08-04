f6ac0a7df67aa98bd30be5286fb16820bd96ddcb
Apple <141291111+LittleVulpine@users.noreply.github.com>
2026-08-04 14:28:44 +1000

    Description widget fix

--- StarPounds/interface/scripted/starpounds/main/descriptionWidgets/breastMilk.lua
+++ StarPounds/interface/scripted/starpounds/main/descriptionWidgets/breastMilk.lua
@@ -1,5 +1,6 @@
 descriptionFunctions.breastMilk = descriptionFunctions.breastMilk or function(descriptionWidget)
   descriptionWidget.onClick = function()
+    local starPounds = getmetatable ''.starPounds
     starPounds.moduleFunc("breasts", "setMilkType", "milk")
   end
 end
